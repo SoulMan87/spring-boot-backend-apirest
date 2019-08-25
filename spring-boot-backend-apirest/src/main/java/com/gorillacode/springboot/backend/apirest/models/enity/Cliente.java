ae5eec1a76428070582cd9dab5f30607695fa359
Soulman87 <nutibarariders@gmail.com>
2019-08-25 16:05:42 -0500

    Second Commit

--- spring-boot-backend-apirest/src/main/java/com/gorillacode/springboot/backend/apirest/models/enity/Cliente.java
+++ spring-boot-backend-apirest/src/main/java/com/gorillacode/springboot/backend/apirest/models/enity/Cliente.java
@@ -13,24 +13,24 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-
 @Entity
-@Table(name="clientes")
-public class Cliente implements Serializable{
-	
-    @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+@Table(name = "clientes")
+public class Cliente implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-    
- 
+
+	@Column(nullable = false)
 	private String nombre;
 	private String apellido;
+	@Column(nullable = false, unique = true)
 	private String email;
-	
-	@Column(name="create_at")
+
+	@Column(name = "create_at")
 	@Temporal(TemporalType.DATE)
 	private Date createAt;
-	
+
 	@PrePersist
 	public void prePersist() {
 		createAt = new Date();
@@ -67,6 +67,7 @@ public class Cliente implements Serializable{
 	public void setCreateAt(Date createAt) {
 		this.createAt = createAt;
 	}
+
 	public String getApellido() {
 		return apellido;
 	}
@@ -74,6 +75,7 @@ public class Cliente implements Serializable{
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
+
 	/**
 	 * 
 	 */
