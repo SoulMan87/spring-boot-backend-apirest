092126f68f9a314024523750c78ffaa8edd45447
Soulman87 <nutibarariders@gmail.com>
2019-10-16 14:09:18 -0500

    paginator

--- spring-boot-backend-apirest/src/main/java/com/gorillacode/springboot/backend/apirest/models/dao/IClienteDao.java
+++ spring-boot-backend-apirest/src/main/java/com/gorillacode/springboot/backend/apirest/models/dao/IClienteDao.java
@@ -1,9 +1,9 @@
 package com.gorillacode.springboot.backend.apirest.models.dao;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.gorillacode.springboot.backend.apirest.models.enity.Cliente;
 
-public interface IClienteDao extends CrudRepository<Cliente, Long> {
+public interface IClienteDao extends JpaRepository<Cliente, Long> {
 
 }
--- spring-boot-backend-apirest/src/main/java/com/gorillacode/springboot/backend/apirest/models/services/ClienteServiceImpl.java
+++ spring-boot-backend-apirest/src/main/java/com/gorillacode/springboot/backend/apirest/models/services/ClienteServiceImpl.java
@@ -3,6 +3,8 @@ package com.gorillacode.springboot.backend.apirest.models.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,21 +12,27 @@ import com.gorillacode.springboot.backend.apirest.models.dao.IClienteDao;
 import com.gorillacode.springboot.backend.apirest.models.enity.Cliente;
 
 @Service
-public class ClienteServiceImpl implements IClienteService{
-	
-	
-    @Autowired
-    private IClienteDao clienteDao;
-    
+public class ClienteServiceImpl implements IClienteService {
+
+	@Autowired
+	private IClienteDao clienteDao;
+
 	@Override
-	@Transactional(readOnly=true)
+	@Transactional(readOnly = true)
 	public List<Cliente> findAll() {
-		
-		return (List<Cliente>)clienteDao.findAll();
+
+		return (List<Cliente>) clienteDao.findAll();
 	}
 
 	@Override
-	@Transactional(readOnly=true)
+	@Transactional(readOnly = true)
+	public Page<Cliente> findAll(Pageable pageable) {
+		
+		return clienteDao.findAll(pageable);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
 	public Cliente findById(Long id) {
 		return clienteDao.findById(id).orElse(null);
 	}
